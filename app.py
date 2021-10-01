ecc846ab938cf49544588bca09fa0644035c2d46
stephan <stephan.dint.mueller@gmail.com>
2021-10-01 22:02:18 +0200

    Migrated the code to supabase

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import json
 import datetime
+from supabase_py import create_client, Client
 
 st.set_page_config(page_title='Liquidationsrechner', page_icon=":ledger:",)
 
@@ -13,6 +14,10 @@ CURRENT_YEAR = datetime.datetime.now().year
 NOTIONAL_PURCHASE_RATE = 0.022
 NOTIONAL_PURCHASE_RATE_MARRIED = 0.02
 OTHER_LIQUIDATION_RATE = 0.04
+APIKEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMjYwNDgyMSwiZXhwIjoxOTQ4MTgwODIxfQ.lGyzXxNAeor9HBd2wcaJ4he1SxMBXEM13PbLmYsxjcM'
+URL = 'https://hbhuxcikiovvpeiwgnqf.supabase.co'
+
+supabase: Client = create_client(URL, APIKEY)
 
 def calculate_simple_tax(married, notional_purchase, other_liquidation_profit):
     notional_purchase_tax = 0
@@ -24,47 +29,36 @@ def calculate_simple_tax(married, notional_purchase, other_liquidation_profit):
     other_liquidation_profit_tax = other_liquidation_profit * OTHER_LIQUIDATION_RATE
     return other_liquidation_profit_tax + notional_purchase_tax
 
-def find_municipality_data(tax_year, municipality):
-    f_tax_rates = open('data.json')
-    tax_rates_all_periods = json.load(f_tax_rates)
-    tax_rates = tax_rates_all_periods[str(tax_year)]
-    for tax_rate in tax_rates:
-        if tax_rate['Commune'] == municipality:
+def find_municipality_data(data, municipality):
+    for tax_rate in data['data']:
+        if tax_rate['Commune Name'] == municipality:
             return tax_rate
 
 def extract_simple_tax_multiplier(municipality_data, married, denomination, denomination_partner):
-    simple_tax_multiplier = municipality_data['Canton_1']
-    simple_tax_multiplier += municipality_data['Commune_1']
-    print('Simple Tax Multiplicator canton and municipality factor: ', simple_tax_multiplier)
+    simple_tax_multiplier = municipality_data['Canton Rate']
+    simple_tax_multiplier += municipality_data['Commune Rate']
     target_data_denomination = transform_input_denomination_to_target_data_denomination(denomination)
     target_data_denomination_partner = transform_input_denomination_to_target_data_denomination(denomination_partner)
-    if target_data_denomination != '' and target_data_denomination_partner != '':
-        if not married:
-            simple_tax_multiplier += municipality_data[target_data_denomination]
-        else:
-            print('Before: ', simple_tax_multiplier)
-            simple_tax_multiplier += (municipality_data[target_data_denomination] + municipality_data[target_data_denomination_partner])/2
-            print('After: ', simple_tax_multiplier)
+    # Denomination tax rate of married couple has to be added and then substracted by 2
+    if not married:
+        simple_tax_multiplier += municipality_data.get(target_data_denomination,0)
+    else:
+        simple_tax_multiplier += (municipality_data.get(target_data_denomination,0) + municipality_data.get(target_data_denomination_partner,0))/2
             
     return simple_tax_multiplier/100
 
-def calculate_federal_tax(file_name, tax_amount):
-    f_federal_tax = open(file_name)
-    federal_tax_rates = json.load(f_federal_tax)
-    f_federal_tax.close()
-
-    target_federal_tax_rate = None
-    for i, tax_rate in enumerate(federal_tax_rates):
-        if tax_amount < tax_rate["income"]:
+def calculate_federal_tax(tax_amount, married):
+    federal_data = supabase.table("Federal Tax Rate").select("*").eq('Married', str(married)).execute()
+    federal_tax_rates = federal_data['data']
+    target_federal_tax_rate = federal_tax_rates[0]
+    for i in range(len(federal_tax_rates)):
+        if i == (len(federal_tax_rates)-1):
             target_federal_tax_rate = federal_tax_rates[i-1]
+        if tax_amount < federal_tax_rates[i]["Income"]:
+            if i != 0:
+                target_federal_tax_rate = federal_tax_rates[i-1]
             break
-        else:
-            continue
-    
-    if target_federal_tax_rate != None:
-        federal_tax = target_federal_tax_rate["tax"] + (int((tax_amount - target_federal_tax_rate["income"])/100) * target_federal_tax_rate["progression"])
-        return federal_tax
-    return 0            
+    return target_federal_tax_rate["Tax"] + (int((tax_amount - target_federal_tax_rate["Income"])/100) * target_federal_tax_rate["Progression"])
 
 def transform_input_denomination_to_target_data_denomination(input_denomination):
     if input_denomination == 'Evangelisch':
@@ -80,7 +74,7 @@ def validate_input():
         st.info('Die Konfession Ehepartner wurde auf konfessionslos gestellt, da verheiratet nicht ausgewählt wurde')
 
 with st.form(key='liquidation_information'):
-    year_input = st.number_input('Steuerjahr', min_value=2020, max_value=2020, value=CURRENT_YEAR-1, step=1)
+    year_input = st.number_input('Steuerjahr', min_value=2019, max_value=2021, value=CURRENT_YEAR-1, step=1)
     municipality = st.selectbox('Gemeinde', municipalities)
     married = st.checkbox('Verheiratet', key='married')
     denomination = st.selectbox('Konfession', ('Konfessionslos', 'Römisch-Katholisch', 'Evangelisch'), key='denomination')
@@ -90,36 +84,35 @@ with st.form(key='liquidation_information'):
     submit_button = st.form_submit_button(label='Berechnen', on_click=validate_input)
 
 if submit_button:
+    data = supabase.table("Swiss Tax Rate").select("*").eq('Canton', 'SG').eq('Tax Year', str(year_input)).execute()
     simple_tax = calculate_simple_tax(married, notional_purchase, other_liquidation_profit)
-    municipality_data = find_municipality_data(year_input, municipality)
+    municipality_data = find_municipality_data(data, municipality)
     simple_tax_multiplier = extract_simple_tax_multiplier(municipality_data, married, denomination, denomination_partner)
     local_tax = simple_tax * simple_tax_multiplier
 
-    if married == False:
-        federal_tax_notional_purchase = calculate_federal_tax('federal_tax.json', notional_purchase)/5
-        federal_tax_other_liquidation_profit = calculate_federal_tax('federal_tax.json', other_liquidation_profit/5)
-    else:
-        federal_tax_notional_purchase = calculate_federal_tax('federal_tax_married.json', notional_purchase)/5
-        federal_tax_other_liquidation_profit = calculate_federal_tax('federal_tax_married.json', other_liquidation_profit/5)
-
-    # Ensure that user input of 0 does not produce divison by 0
-    other_liquidation_profit_divider = 0
+    federal_tax_other_liquidation_profit = calculate_federal_tax(other_liquidation_profit/5, married)
+    # Ensure 0 input does not result in division by 0
     if other_liquidation_profit == 0:
         other_liquidation_profit_divider = 1
     else:
         other_liquidation_profit_divider = other_liquidation_profit
-
-    if federal_tax_other_liquidation_profit / other_liquidation_profit_divider < 0.02:
-        federal_tax_other_liquidation_profit = other_liquidation_profit * 0.02
     
-    federal_tax = federal_tax_notional_purchase + federal_tax_other_liquidation_profit
+    federal_tax_other_liquidation_profit_rate = federal_tax_other_liquidation_profit / (other_liquidation_profit_divider/5)
 
-    print('Simple Tax', simple_tax)
-    print('Local Tax: ', local_tax)
-    print('Federal Tax: ', federal_tax)
-    print('Multiplier: ', simple_tax_multiplier)
+    print('federal tax other liquidation profit rate: ', federal_tax_other_liquidation_profit_rate)
+
+    if federal_tax_other_liquidation_profit_rate < 0.02:
+        federal_tax_other_liquidation_profit_rate = 0.02
+
+    federal_tax_other_liquidation_profit = other_liquidation_profit * federal_tax_other_liquidation_profit_rate
+    print('Federal tax other liquidation profit: ', federal_tax_other_liquidation_profit)
+    federal_tax_notional_purchase = calculate_federal_tax(notional_purchase, married)/5
+    print('Federal tax notional purchase: ', federal_tax_notional_purchase)
+    federal_tax = federal_tax_notional_purchase + federal_tax_other_liquidation_profit
 
     col1, col2 = st.columns(2)
     col1.metric(label='Kantons- und Gemeindesteuern', value="CHF {:,.2f}".format(local_tax))
     col2.metric(label='Bundessteuern', value="CHF {:,.2f}".format(federal_tax))
+
+    print(data)
 
