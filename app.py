fcb05a9684e077906632c61c0170273fa866bb54
stephan <stephan.dint.mueller@gmail.com>
2021-10-04 21:02:29 +0200

    Minor change

--- app.py
+++ app.py
@@ -20,34 +20,6 @@ st.markdown(f""" <style>
         padding-left: {padding}rem;
         padding-bottom: {padding}rem;
     }} </style> """, unsafe_allow_html=True)
-
-def set_page_title(title):
-    st.sidebar.markdown(unsafe_allow_html=True, body=f"""
-        <iframe height=0 srcdoc="<script>
-            const title = window.parent.document.querySelector('title') \
-                
-            const oldObserver = window.parent.titleObserver
-            if (oldObserver) {{
-                oldObserver.disconnect()
-            }} \
-
-            const newObserver = new MutationObserver(function(mutations) {{
-                const target = mutations[0].target
-                if (target.text !== '{title}') {{
-                    target.text = '{title}'
-                }}
-            }}) \
-
-            newObserver.observe(title, {{ childList: true }})
-            window.parent.titleObserver = newObserver \
-
-            title.text = '{title}'
-        </script>" />
-    """)
-
-
-set_page_title("Liquidationskalkulator")
-
 st.title('Inoffizieller Liquidationsrechner Kanton St. Gallen')
 
 f_municipalities = open('municipalities.json')
