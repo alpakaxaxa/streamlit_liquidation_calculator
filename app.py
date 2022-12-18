50efe1824e79d6feb60c5d3b69b29108d978c4ab
stephan <stephan.dint.mueller@gmail.com>
2022-12-18 17:45:55 +0100

    added year 2022

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import datetime
 from supabase_py import create_client, Client
 
 st.set_page_config(page_title='Liquidationsrechner', page_icon=":ledger:",)
-hide_streamlit_style='''
+hide_streamlit_style = '''
 <style>
     #MainMenu{visibility:hidden;}
     footer{visibility:hidden;}
 </style>
 '''
-st.markdown(hide_streamlit_style,unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.title('Inoffizieller Liquidationsrechner Kanton St. Gallen')
 
@@ -25,33 +25,41 @@ URL = 'https://hbhuxcikiovvpeiwgnqf.supabase.co'
 
 supabase: Client = create_client(URL, st.secrets["APIKEY"])
 
+
 def calculate_simple_tax(married, notional_purchase, other_liquidation_profit):
     notional_purchase_tax = 0
     if married:
         notional_purchase_tax += notional_purchase * NOTIONAL_PURCHASE_RATE_MARRIED
     else:
         notional_purchase_tax += notional_purchase * NOTIONAL_PURCHASE_RATE
-    
+
     other_liquidation_profit_tax = other_liquidation_profit * OTHER_LIQUIDATION_RATE
     return other_liquidation_profit_tax + notional_purchase_tax
+
 
 def find_municipality_data(data, municipality):
     for tax_rate in data['data']:
         if tax_rate['Commune Name'] == municipality:
             return tax_rate
 
+
 def extract_simple_tax_multiplier(municipality_data, married, denomination, denomination_partner):
     simple_tax_multiplier = municipality_data['Canton Rate']
     simple_tax_multiplier += municipality_data['Commune Rate']
-    target_data_denomination = transform_input_denomination_to_target_data_denomination(denomination)
-    target_data_denomination_partner = transform_input_denomination_to_target_data_denomination(denomination_partner)
+    target_data_denomination = transform_input_denomination_to_target_data_denomination(
+        denomination)
+    target_data_denomination_partner = transform_input_denomination_to_target_data_denomination(
+        denomination_partner)
     # Denomination tax rate of married couple has to be added and then substracted by 2
     if not married:
-        simple_tax_multiplier += municipality_data.get(target_data_denomination,0)
+        simple_tax_multiplier += municipality_data.get(
+            target_data_denomination, 0)
     else:
-        simple_tax_multiplier += (municipality_data.get(target_data_denomination,0) + municipality_data.get(target_data_denomination_partner,0))/2
-            
+        simple_tax_multiplier += (municipality_data.get(target_data_denomination, 0) +
+                                  municipality_data.get(target_data_denomination_partner, 0))/2
+
     return simple_tax_multiplier/100
+
 
 @st.cache
 def calculate_federal_tax(tax_amount, married):
@@ -67,6 +75,7 @@ def calculate_federal_tax(tax_amount, married):
             break
     return target_federal_tax_rate["Tax"] + (int((tax_amount - target_federal_tax_rate["Income"])/100) * target_federal_tax_rate["Progression"])
 
+
 def transform_input_denomination_to_target_data_denomination(input_denomination):
     if input_denomination == 'Evangelisch':
         return 'Church, Protestant'
@@ -75,10 +84,12 @@ def transform_input_denomination_to_target_data_denomination(input_denomination)
     else:
         return ''
 
+
 def validate_input():
     if st.session_state.married == False and st.session_state.denomination_partner != 'Konfessionslos':
         st.session_state.denomination_partner = 'Konfessionslos'
         st.info('Die Konfession Ehepartner wurde auf konfessionslos gestellt, da verheiratet nicht ausgewählt wurde')
+
 
 @st.cache
 def get_tax_data():
@@ -89,39 +100,54 @@ def get_tax_data():
 def get_federal_tax_data():
     return supabase.table("Federal Tax Rate").select("*").eq('Married', str(married)).execute()
 
+
 with st.form(key='liquidation_information'):
-    year_input = st.number_input('Steuerjahr', min_value=2019, max_value=2021, value=CURRENT_YEAR-1, step=1)
+    year_input = st.number_input(
+        'Steuerjahr', min_value=2019, max_value=2022, value=CURRENT_YEAR-1, step=1)
     municipality = st.selectbox('Gemeinde', municipalities)
     married = st.checkbox('Verheiratet', key='married')
-    denomination = st.selectbox('Konfession', ('Konfessionslos', 'Römisch-Katholisch', 'Evangelisch'), key='denomination')
-    denomination_partner = st.selectbox('Konfession Ehepartner', ('Konfessionslos', 'Römisch-Katholisch', 'Evangelisch'), key='denomination_partner')
-    notional_purchase = st.number_input('Fiktiver Einkauf', step=1, key='notional_purchase')
-    other_liquidation_profit = st.number_input('Übriger Liquidationsgewinn', step=1, key='other_liquidation_profit')
-    submit_button = st.form_submit_button(label='Berechnen', on_click=validate_input)
+    denomination = st.selectbox(
+        'Konfession', ('Konfessionslos', 'Römisch-Katholisch', 'Evangelisch'), key='denomination')
+    denomination_partner = st.selectbox('Konfession Ehepartner', (
+        'Konfessionslos', 'Römisch-Katholisch', 'Evangelisch'), key='denomination_partner')
+    notional_purchase = st.number_input(
+        'Fiktiver Einkauf', step=1, key='notional_purchase')
+    other_liquidation_profit = st.number_input(
+        'Übriger Liquidationsgewinn', step=1, key='other_liquidation_profit')
+    submit_button = st.form_submit_button(
+        label='Berechnen', on_click=validate_input)
 
 if submit_button:
     data = get_tax_data()
-    simple_tax = calculate_simple_tax(married, notional_purchase, other_liquidation_profit)
+    simple_tax = calculate_simple_tax(
+        married, notional_purchase, other_liquidation_profit)
     municipality_data = find_municipality_data(data, municipality)
-    simple_tax_multiplier = extract_simple_tax_multiplier(municipality_data, married, denomination, denomination_partner)
+    simple_tax_multiplier = extract_simple_tax_multiplier(
+        municipality_data, married, denomination, denomination_partner)
     local_tax = simple_tax * simple_tax_multiplier
 
-    federal_tax_other_liquidation_profit = calculate_federal_tax(other_liquidation_profit/5, married)
+    federal_tax_other_liquidation_profit = calculate_federal_tax(
+        other_liquidation_profit/5, married)
     # Ensure 0 input does not result in division by 0
     if other_liquidation_profit == 0:
         other_liquidation_profit_divider = 1
     else:
         other_liquidation_profit_divider = other_liquidation_profit
-    
-    federal_tax_other_liquidation_profit_rate = federal_tax_other_liquidation_profit / (other_liquidation_profit_divider/5)
+
+    federal_tax_other_liquidation_profit_rate = federal_tax_other_liquidation_profit / \
+        (other_liquidation_profit_divider/5)
 
     if federal_tax_other_liquidation_profit_rate < 0.02:
         federal_tax_other_liquidation_profit_rate = 0.02
 
-    federal_tax_other_liquidation_profit = other_liquidation_profit * federal_tax_other_liquidation_profit_rate
-    federal_tax_notional_purchase = calculate_federal_tax(notional_purchase, married)/5
-    federal_tax = federal_tax_notional_purchase + federal_tax_other_liquidation_profit
+    federal_tax_other_liquidation_profit = other_liquidation_profit * \
+        federal_tax_other_liquidation_profit_rate
+    federal_tax_notional_purchase = calculate_federal_tax(
+        notional_purchase, married)/5
+    federal_tax = federal_tax_notional_purchase + \
+        federal_tax_other_liquidation_profit
 
     col1, col2 = st.columns(2)
-    col1.metric(label='Kantons- und Gemeindesteuern', value="CHF {:,.2f}".format(local_tax))
+    col1.metric(label='Kantons- und Gemeindesteuern',
+                value="CHF {:,.2f}".format(local_tax))
     col2.metric(label='Bundessteuern', value="CHF {:,.2f}".format(federal_tax))
